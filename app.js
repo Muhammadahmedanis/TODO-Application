84cfb8ce0fc6fc1e655f5f923375cfc540085d7e
ahmedanis45 <ahmedanis4546@gmail.com>
2024-05-23 23:47:22 +0500

    updateUI

--- app.js
+++ app.js
@@ -2,55 +2,114 @@ var ul = document.getElementById("ul");
 var inp = document.getElementById("inp");
 
 function add() {
-var li = document.createElement("li");
-var liText = document.createTextNode(inp.value);
-li.appendChild(liText);
-ul.appendChild(li)
-inp.value ="";
-var updBtn = document.createElement("button");
-updBtn.classList.add("btnStyle");
-var updText =  document.createTextNode("Update");
-updBtn.appendChild(updText);
-li.appendChild(updBtn);
-updBtn.setAttribute("onclick", "upd(this)");
+    var li = document.createElement("li");
+    li.setAttribute("class", "list");
+    var liText = document.createTextNode(inp.value);
+    li.appendChild(liText);
+    ul.appendChild(li);
 
-
-var delBtn = document.createElement("button");
-delBtn.classList.add("btnStyle2");
-var delText = document.createTextNode("Delete");
-delBtn.appendChild(delText);
-li.appendChild(delBtn);
-delBtn.setAttribute("onclick", "del(this)");
-
-}
-
-// update function
-function upd(e) {
-    inp.value = e.parentNode.firstChild.nodeValue;
-    var edit = document.createElement("button");
-    edit.classList.add("editBtn")
-    var editText = document.createTextNode("Edit");
-    edit.appendChild(editText);
-    e.parentNode.appendChild(edit);
-    edit.setAttribute("onclick", "editFunc(this)");
-    document.querySelector(".btn").disabled = true;
-}
-
-
-// edit function
-function editFunc(e) {
-    e.parentNode.firstChild.nodeValue = inp.value;
-    document.querySelector(".btn").disabled = false;
     inp.value = "";
-    e.remove();
+    
+    var updBtn = document.createElement("i");
+    updBtn.setAttribute("class", "fa-solid fa-pencil");
+    li.appendChild(updBtn);
+    updBtn.setAttribute("onclick", "updBtn(this)");
+    
+    
+    var delBtn =document.createElement("i");
+    delBtn.setAttribute("class", "fa-regular fa-trash-can");
+    li.appendChild(delBtn);
+    delBtn.setAttribute("onclick", "delFunc(this)");
+    
+    var di = document.createElement("span");
+    di.setAttribute("class", "fi");
+    di.appendChild(updBtn);
+    di.appendChild(delBtn);
+    li.appendChild(di);
+
+
+
 }
 
-// delete function
-function del(e) {
-    e.parentNode.remove();
+// delBtn
+function delFunc(e) {
+    e.parentNode.parentNode.remove();
 }
 
-// delete All function
-function delAll() {
-    ul.innerHTML = "";   
+function updBtn(e) {
+    inp.value = e.parentNode.parentNode.textContent;
+    var ed = document.createElement("i");
+    ed.setAttribute("class", "fa-solid fa-spinner");
+    document.querySelector(".span").disabled = true;
+    e.parentNode.firstChild.disaply = "none";
+    e.parentNode.appendChild(ed);
+    ed.setAttribute("onclick", "ad1(this)")
 }
+
+function ad1(e) {
+    e.parentNode.parentNode.firstChild.textContent = inp.value;
+    // e.parentNode.firstChild.disabled = 
+    inp.value = "";
+    e.parentNode.lastChild.remove();
+    document.querySelector(".span").disabled = false;
+}
+
+
+
+
+// var ul = document.getElementById("ul");
+// var inp = document.getElementById("inp");
+
+// function add() {
+// var li = document.createElement("li");
+// var liText = document.createTextNode(inp.value);
+// li.appendChild(liText);
+// ul.appendChild(li)
+// inp.value ="";
+// var updBtn = document.createElement("button");
+// updBtn.classList.add("btnStyle");
+// var updText =  document.createTextNode("Update");
+// updBtn.appendChild(updText);
+// li.appendChild(updBtn);
+// updBtn.setAttribute("onclick", "upd(this)");
+
+
+// var delBtn = document.createElement("button");
+// delBtn.classList.add("btnStyle2");
+// var delText = document.createTextNode("Delete");
+// delBtn.appendChild(delText);
+// li.appendChild(delBtn);
+// delBtn.setAttribute("onclick", "del(this)");
+
+// }
+
+// // update function
+// function upd(e) {
+//     inp.value = e.parentNode.firstChild.nodeValue;
+//     var edit = document.createElement("button");
+//     edit.classList.add("editBtn")
+//     var editText = document.createTextNode("Edit");
+//     edit.appendChild(editText);
+//     e.parentNode.appendChild(edit);
+//     edit.setAttribute("onclick", "editFunc(this)");
+//     document.querySelector(".btn").disabled = true;
+// }
+
+
+// // edit function
+// function editFunc(e) {
+//     e.parentNode.firstChild.nodeValue = inp.value;
+//     document.querySelector(".btn").disabled = false;
+//     inp.value = "";
+//     e.remove();
+// }
+
+// // delete function
+// function del(e) {
+//     e.parentNode.remove();
+// }
+
+// // delete All function
+// function delAll() {
+//     ul.innerHTML = "";   
+// }
